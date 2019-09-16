fc68aaa07b4cb7b7327616882a00ae212531ad29
Rodrigo Damaceno dos Santos <rodigozeus@hotmail.com>
2019-09-16 17:03:38 -0300

    OBR

--- Códigos e testes/Open Cv - Testes/Exemplo.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-imagemCarregada = cv2.imread("direita.jpg", 0)
-
-cv2.imshow("ImagemCarregada", imagemCarregada)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imwrite("imagemSalva.jpg", imagemCarregada)
