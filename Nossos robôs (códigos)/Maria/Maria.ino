9eff8cbf299d8bebe6e303b57d70600737dd34b4
Rodrigo Damaceno dos Santos <rodigozeus@hotmail.com>
2019-12-12 10:17:43 -0300

    Update Maria.ino

--- Nossos robôs (códigos)/Maria/Maria.ino
+++ Nossos robôs (códigos)/Maria/Maria.ino
@@ -1,19 +1,37 @@
 /*
+ * Olá Maria,
+ * Abaixo segue um esboço de código para controlar o _________
+ * 
+ * Lhe proponho uma serie de desafios:
+ *    1 - Fazer com que o robô gire tanto pra direita quanto pra esquerda
+ *    2 - Construir uma pista de linha com curvas arredondadas (baixa angulação)
+ *    3 - Instalar dois sensores de linha no ________
+ *    4 - Realizar testes com os sensores de linha e definir os valores de corte
+ *    5 - Tornar o _________ capaz de seguir a linha.
+ *    
+ * Não são desafios fáceis. (não mesmo rsrs)
+ * Mas, tenho certeza de que você consegue!!! :)
+ */
+
+
+
+//Biblioteca do Shield Ponte-H (esta plaquinha em cima do arduíno)
+#include <AFMotor.h>
+
+/*
 ==============================================================================================================
 MAPEAMENTO DE HARDWARE:
 ==============================================================================================================*/
-#include <AFMotor.h>
 
-AF_DCMotor motor_esquerdo(1);
-AF_DCMotor motor_direito(4);
-
+AF_DCMotor motor_esquerdo(1); //motor 1
+AF_DCMotor motor_direito(4);  //motor 4
 
 /*
 ==============================================================================================================
 CONSTANTES e VARIÁVEIS:
 ==============================================================================================================*/
 
-
+  //nada por enquanto
 
 /*
 ==============================================================================================================
@@ -21,7 +39,7 @@ CONFIGURAÇÃO:
 ==============================================================================================================*/
 void setup() {
   
-  Serial.begin(9600);
+  //nada por enquanto
     
 }
 
@@ -32,10 +50,15 @@ PROGRAMA PRINCIPAL:
 ==============================================================================================================*/
 void loop() {
 
+//vai pra frente com 100 de velocidade (máximo é 255)
 frente(100);
 delay(500);
+
+
 para();
 delay(500);
+
+//vai pra tras com 100 de velocidade (máximo é 255)
 tras(100);
 delay(500);
 
@@ -48,8 +71,11 @@ DEFININDO CADA FUNÇÃO:
 
 void frente(int velocidade){
   
+  //define a velocidade de cada motor
   motor_esquerdo.setSpeed(velocidade);
   motor_direito.setSpeed(velocidade);
+  
+  //manda o motor girar pra frente (FORWARD)
   motor_esquerdo.run(FORWARD);
   motor_direito.run(FORWARD);
  
@@ -57,8 +83,11 @@ void frente(int velocidade){
 
 void tras(int velocidade){
 
+  //define a velocidade de cada motor
   motor_esquerdo.setSpeed(velocidade);
   motor_direito.setSpeed(velocidade);
+  
+  //manda o motor girar pra tras (BACKWARD)
   motor_esquerdo.run(BACKWARD);
   motor_direito.run(BACKWARD);
 
@@ -66,7 +95,8 @@ void tras(int velocidade){
 
 
 void para(){
-  
+
+  //manda os motores pararem (RELEASE)
   motor_esquerdo.run(RELEASE);
   motor_direito.run(RELEASE);
   
